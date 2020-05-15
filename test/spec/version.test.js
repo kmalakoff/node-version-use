32c2e86dd140ed2a8d4e8e2e939e50be11dab4f9
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-14 21:37:06 -0700

    Got working on windows

--- test/spec/version.test.js
+++ test/spec/version.test.js
@@ -2,38 +2,48 @@ var assert = require('assert');
 
 var nvu = require('../..');
 
+var NODE = process.platform === 'win32' ? 'node.exe' : 'node';
+var EOL = /\r\n|\r|\n/;
 var now = new Date(Date.parse('2020-05-10T03:23:29.347Z'));
 
 describe('versions', function () {
   describe('happy path', function () {
-    it('one version', function (done) {
-      nvu('14', 'node', ['--version'], { stdout: 'string', now: now }, function (err, res) {
+    it('npm whoami', function (done) {
+      nvu('12', 'npm', ['whoami'], { stdout: 'string', now: now }, function (err, res) {
         assert.ok(!err);
-        assert.equal(res.stdout.split('\n')[0], 'v14.2.0');
+        assert.ok(res.stdout.split(EOL).slice(-2, 1)[0].length > 1);
+        done();
+      });
+    });
+
+    it('12', function (done) {
+      nvu('12', NODE, ['--version'], { stdout: 'string', now: now }, function (err, res) {
+        assert.ok(!err);
+        assert.equal(res.stdout.split(EOL).slice(-2, 1)[0], 'v12.16.3');
         done();
       });
     });
 
     it('latest version', function (done) {
-      nvu('latest', 'node', ['--version'], { stdout: 'string', now: now }, function (err, res) {
+      nvu('latest', NODE, ['--version'], { stdout: 'string', now: now }, function (err, res) {
         assert.ok(!err);
-        assert.equal(res.stdout.split('\n')[0], 'v13.14.0');
+        assert.equal(res.stdout.split(EOL).slice(-2, 1)[0], 'v13.14.0');
         done();
       });
     });
 
     it('lts version', function (done) {
-      nvu('lts', 'node', ['--version'], { stdout: 'string', now: now }, function (err, res) {
+      nvu('lts/erbium', NODE, ['--version'], { stdout: 'string', now: now }, function (err, res) {
         assert.ok(!err);
-        assert.equal(res.stdout.split('\n')[0], 'v14.2.0');
+        assert.equal(res.stdout.split(EOL).slice(-2, 1)[0], 'v12.16.3');
         done();
       });
     });
 
     it('lts/argon version', function (done) {
-      nvu('lts/argon', 'node', ['--version'], { stdout: 'string', now: now }, function (err, res) {
+      nvu('lts/argon', NODE, ['--version'], { stdout: 'string', now: now }, function (err, res) {
         assert.ok(!err);
-        assert.equal(res.stdout.split('\n')[0], 'v4.9.1');
+        assert.equal(res.stdout.split(EOL).slice(-2, 1)[0], 'v4.9.1');
         done();
       });
     });
@@ -41,28 +51,28 @@ describe('versions', function () {
 
   describe('unhappy path', function () {
     it('no versions (undefined)', function (done) {
-      nvu(undefined, 'node', ['--version'], { stdout: 'string' }, function (err) {
+      nvu(undefined, NODE, ['--version'], { stdout: 'string' }, function (err) {
         assert.ok(!!err);
         done();
       });
     });
 
     it('no versions (null)', function (done) {
-      nvu(null, 'node', ['--version'], { stdout: 'string' }, function (err) {
+      nvu(null, NODE, ['--version'], { stdout: 'string' }, function (err) {
         assert.ok(!!err);
         done();
       });
     });
 
     it('invalid versions', function (done) {
-      nvu('1.d.4', 'node', ['--version'], { stdout: 'string' }, function (err) {
+      nvu('1.d.4', NODE, ['--version'], { stdout: 'string' }, function (err) {
         assert.ok(!!err);
         done();
       });
     });
 
     it('invalid versions', function (done) {
-      nvu('bob', 'node', ['--version'], { stdout: 'string' }, function (err) {
+      nvu('bob', NODE, ['--version'], { stdout: 'string' }, function (err) {
         assert.ok(!!err);
         done();
       });
