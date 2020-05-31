a546b1a9cd24f6b5a35f813d64eac239edb61808
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-31 08:59:52 -0700

    fix dependencies

---
+++ bin/node-version-use.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var getopts = require('getopts-compat');
+
+(function () {
+  var options = getopts(process.argv.slice(3), {
+    stopEarly: true,
+  });
+
+  var args = process.argv.slice(2, 3).concat(options._);
+  if (args.length < 1) {
+    console.log('err command. Example usage: nvu version [command]');
+    return process.exit(-1);
+  }
+  var nvu = require('..');
+
+  nvu(args[0], args[1], args.slice(2), { stdio: 'inherit' }, function (err) {
+    if (err) {
+      console.log(err.message);
+      return process.exit(err.code || -1);
+    }
+    process.exit(0);
+  });
+})();
