b823363381f02faf18a3ce2f70a1a9e766f5c1ad
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-31 17:52:34 -0700

    Added safe exit

--- bin/node-version-use.js
+++ bin/node-version-use.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var getopts = require('getopts-compat');
+var exit = require('exit');
+var nvu = require('..');
 
 (function () {
   var options = getopts(process.argv.slice(3), {
@@ -10,14 +12,14 @@ var getopts = require('getopts-compat');
   var args = process.argv.slice(2, 3).concat(options._);
   if (args.length < 1) {
     console.log('err command. Example usage: nvu version [command]');
-    return process.exit(-1);
+    return exit(-1);
   }
-  var nvu = require('..');
 
   nvu(args[0], args[1], args.slice(2), { stdio: 'inherit' }, function (err) {
     if (err) {
       console.log(err.message);
-      return process.exit(err.code || -1);
+      return exit(err.code || -1);
     }
+    exit(0);
   });
 })();
