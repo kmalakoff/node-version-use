1874b1bdbbad958d4ab4850c7baf47b521af6c10
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-21 19:01:58 -0800

    Simplify spawn options passing in use.js

--- src/use.js
+++ src/use.js
@@ -3,19 +3,12 @@ const Queue = require('queue-cb');
 const installRelease = require('node-install-release');
 const versionUtils = require('node-version-utils');
 const resolveVersions = require('node-resolve-versions');
-
 const constants = require('./constants');
-const spawnKeys = ['encoding', 'stdio', 'stdin', 'stdout', 'stderr', 'cwd', 'env'];
 
 module.exports = function use(versionExpression, command, args, options, callback) {
   resolveVersions(versionExpression, { ...options, path: 'raw' }, (err, versions) => {
     if (err) return callback(err);
     if (!versions.length) return callback(new Error(`No versions found from expression: ${versionExpression}`));
-
-    const spawnOptions = {};
-    for (let i = 0; i < spawnKeys.length; i++) {
-      if (options[spawnKeys[i]] !== undefined) spawnOptions[spawnKeys[i]] = options[spawnKeys[i]];
-    }
 
     const results = [];
     const queue = new Queue(1);
@@ -31,7 +24,7 @@ module.exports = function use(versionExpression, command, args, options, callbac
           installRelease(version, installPath, { cacheDirectory: cacheDirectory }, (err) => {
             if (err) return callback(err);
 
-            versionUtils.spawn(installPath, command, args, spawnOptions, (err, res) => {
+            versionUtils.spawn(installPath, command, args, options, (err, res) => {
               results.push({ version: version.version, error: err, result: res });
               callback();
             });
