10766fffdab8501ad8ce3f6353fc2a1c634cfefd
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-09 21:43:04 -0800

    Upgrade modules

--- dist/cjs/use.js
+++ dist/cjs/use.js
@@ -93,4 +93,4 @@ module.exports = function use(versionExpression, command, args, options, callbac
         });
     });
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
