81ddd84c29097403c58f3c058d089fdb7ca43ee8
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-11 21:38:26 -0800

    Upgrade modules

--- scripts/postinstall.cjs
+++ scripts/postinstall.cjs
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-require('../dist/cjs/scripts/postinstall.js');
+var fs = require('fs');
+var path = require('path');
+var compiled = path.join(__dirname, '..', 'dist', 'cjs', 'scripts', 'postinstall.js');
+if (fs.existsSync(compiled)) {
+  require(compiled);
+} else {
+  console.log('postinstall: Skipping (dist/ not built yet - run npm run build)');
+}
