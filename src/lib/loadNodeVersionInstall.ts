9adb3e8519fd16b3161eb8aad8c47dab12c577ee
Kevin Malakoff <kmalakoff@gmail.com>
2026-08-31 00:10:55 -0700

    Update package dependencies and refactor import statements for compatibility

--- src/lib/loadNodeVersionInstall.ts
+++ src/lib/loadNodeVersionInstall.ts
@@ -1,4 +1,4 @@
-import installModule from 'install-module-linked';
+import installModule from 'install-module-linked-compat';
 import type { InstallOptions, InstallResult } from 'node-version-install';
 import path from 'path';
 import url from 'url';
